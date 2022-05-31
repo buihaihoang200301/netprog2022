1c0aa627c7101f89ada1724fddbef88daf0c1ecf
buihaihoang200301 <hoangbh.ba10-022@st.usth.edu.vn>
2022-05-31 10:34:29 +0700

    Prac3

--- 03.practical.work.server.setup.c
+++ 03.practical.work.server.setup.c
@@ -9,28 +9,31 @@ int main () {
     int sockfd, clen, clientfd;
     struct sockaddr_in saddr, caddr;
     unsigned short port = 8784;
-
+	
     if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("Error");
-
+	//setup 
     }
     memset(&saddr, 0, sizeof(saddr));
     saddr.sin_family = AF_INET;
     saddr.sin_addr.s_addr = htonl(INADDR_ANY);
     saddr.sin_port = htons(port);
+    //bind 
     if (bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
 	    printf("Error");
-
+	//listen 
     }
     if (listen(sockfd, 5) < 0) {
         printf("Error");
 
     }
-    
+    //accept 
     clen=sizeof(caddr);
 
     if ((clientfd=accept(sockfd, (struct sockaddr *) &caddr, &clen)) < 0) {
         printf("Error");
 
     }
+    printf("Connected");
+    return 0;
  }
